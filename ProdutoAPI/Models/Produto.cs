72f5e512cbe86179ce85354c76b707048c01ea6c
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-21 22:57:17 -0300

    feat: added Produto.cs

---
+++ ProdutoAPI/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProdutoAPI.Models
+{
+    public class Produto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O campo descrição é obrigatório",AllowEmptyStrings =false)]
+        [StringLength(200,ErrorMessage ="Descrição máximo de 200 caracteres")]
+        public string Descricao { get; set; }
+    }
+}
