c5fb6d5d31a97b81b1e6cd305d53e399c0fefad4
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-21 23:10:37 -0300

    feat: added ProdutoContext.cs

---
+++ ProdutoAPI/Data/ProdutoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutoAPI.Models;
+
+namespace ProdutoAPI.Data
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
